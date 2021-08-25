6e26749e5ab5b7bebf9355cd57e08bf413cca54d
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-25 13:46:22 +0300

    get input and controll form

--- src/components/Profile/ProfileForm.jsx
+++ src/components/Profile/ProfileForm.jsx
@@ -1,17 +1,37 @@
 import classes from './ProfileForm.module.css';
+import { useState, useContext } from 'react';
+import AuthContext from '../../store/auth-context';
+import { sendData } from '../../utils/http';
 
 const ProfileForm = () => {
+  const authCtx = useContext(AuthContext);
   // gauti input reiksme
+  const [newPassword, setNewPassword] = useState('');
 
   // perimti formos valdyma
-
+  const submitHandler = (e) => {
+    e.preventDefault();
+    // galima validacija
+    console.log('new pass is ', newPassword);
+  };
   // issiusti POST request
   //https://identitytoolkit.googleapis.com/v1/accounts:update?key=[API_KEY]
+  sendData('https://identitytoolkit.googleapis.com/v1/accounts:update?key=', {
+    idToken: authCtx.token,
+    password: newPassword,
+    returnSecureToken: true,
+  });
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
+          type='password'
+          id='new-password'
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
