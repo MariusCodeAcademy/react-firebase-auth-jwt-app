e93d9abf37d146e47af8adc5acc62b8741a72936
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-25 10:01:49 +0300

    sign up works no feedback

--- src/components/Auth/AuthForm.jsx
+++ src/components/Auth/AuthForm.jsx
@@ -1,25 +1,74 @@
+import axios from 'axios';
 import { useState } from 'react';
 
 import classes from './AuthForm.module.css';
+import { apiKey } from '../../config';
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    console.log('sending');
+
+    // paimti email ir password ir siusti i endpoint
+    // https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]
+
+    if (isLogin) {
+      // Prijungti esama vartotoja
+      console.log('Login action');
+      return;
+    }
+    if (!isLogin) {
+      // SUkurti vartotja
+      const url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        apiKey;
+      console.log('Sign up action');
+      console.log(enteredEmail, enteredPassword);
+      // galima validacija
+      const response = await axios.post(url, {
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      });
+      console.log('response', response);
+      return;
+    }
+    // gauti email ir slaptazodi ir pateikti issiuntimu
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input
+            type='email'
+            id='email'
+            required
+            value={enteredEmail}
+            onChange={(event) => setEnteredEmail(event.target.value)}
+          />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input
+            type='password'
+            id='password'
+            required
+            minLength='6'
+            value={enteredPassword}
+            onChange={(event) => setEnteredPassword(event.target.value)}
+          />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
